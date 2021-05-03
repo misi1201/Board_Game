58c1d6e3a57bd86c0a5576f65146ead0b783a55b
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-03 19:23:09 +0200

    Using App class instead of HelloWorldApplication class

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -6,11 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class HelloWorldApplication extends Application {
+public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(HelloWorldApplication.class.getResource("/fxml/Game.fxml"));
+        Parent root = FXMLLoader.load(App.class.getResource("/fxml/Game.fxml"));
         primaryStage.setTitle("Hello, World!");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -5,7 +5,7 @@ import javafx.application.Application;
 public class Main {
 
     public static void main(String[] args) {
-        Application.launch(HelloWorldApplication.class, args);
+        Application.launch(App.class, args);
     }
 
 }
