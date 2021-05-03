7fe039f7548616c712966f0bac3de1ca4bd61c1c
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-03 19:03:39 +0200

    Initial commit

---
+++ src/main/java/hello/HelloWorldApplication.java
@@ -0,0 +1,19 @@
+package hello;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloWorldApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(HelloWorldApplication.class.getResource("/fxml/HelloWorld.fxml"));
+        primaryStage.setTitle("Hello, World!");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+}
