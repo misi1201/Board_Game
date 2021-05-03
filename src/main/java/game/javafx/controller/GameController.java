486186a109174a786fa586400a890f4d66203352
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-03 19:49:09 +0200

    Adding GameController class to Game.fxml

--- src/main/java/game/javafx/controller/GameController.java
+++ src/main/java/game/javafx/controller/GameController.java
@@ -1,4 +1,19 @@
 package game.javafx.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 public class GameController {
+
+    @FXML
+    Pane pane;
+
+
+    @FXML
+    public void initialize(){
+
+
+
+    }
+
 }
