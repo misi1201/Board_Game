d9c85e024facf5aeca90cfee0e58773afa7cfc16
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-03 20:12:53 +0200

    Implementing initialize method

--- src/main/java/game/javafx/controller/GameController.java
+++ src/main/java/game/javafx/controller/GameController.java
@@ -2,6 +2,8 @@ package game.javafx.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class GameController {
 
@@ -11,7 +13,14 @@ public class GameController {
 
     @FXML
     public void initialize(){
-
+        for(int i = 0; i < 200; i+= 40){
+            for(int j = 0; j < 200; j+= 40){
+                Rectangle r = new Rectangle(i,j,40,40);
+                r.setFill(Color.WHITE);
+                r.setStroke(Color.BLACK);
+                pane.getChildren().add(r);
+            }
+        }
 
 
     }
