c805dc1097b85f481012bba68e59d7e870983795
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-08 11:07:08 +0200

    Implementing isValidMove method

--- src/main/java/game/model/GameModel.java
+++ src/main/java/game/model/GameModel.java
@@ -84,4 +84,89 @@ public class GameModel {
     }
 
 
+    public boolean isValidMove(int playerId, int row, int col, int direction) {
+
+        boolean isValid = false;
+
+        if (playerId == 1 && grid[row][col] == 2 || !(playerId == 1) && grid[row][col] == 1) {
+            System.out.println("Nem a te köröd!");
+            isValid = false;
+        } else {
+            try {
+                if (playerId == 1) {
+                    switch (direction) {
+                        case RIGHT: {
+                            if (grid[row][col + 1] == 0) {
+                                isValid = true;
+                            }
+                            break;
+                        }
+                        case LEFT: {
+                            if (grid[row][col - 1] == 0) {
+                                isValid = true;
+                            }
+                            break;
+                        }
+                        case UP: {
+                            if (grid[row - 1][col] == 0) {
+                                isValid = true;
+                            }
+                            break;
+                        }
+                        case DOWN: {
+                            if (grid[row + 1][col] == 0) {
+                                isValid = true;
+                            }
+                            break;
+                        }
+                        default:
+                            isValid = false;
+                    }
+                } else {
+                    if (playerId == 2) {
+                        switch (direction) {
+                            case RIGHT: {
+                                if (grid[row][col + 1] == 0) {
+                                    isValid = true;
+                                }
+                                break;
+                            }
+                            case LEFT: {
+                                if (grid[row][col - 1] == 0) {
+                                    isValid = true;
+                                }
+                                break;
+                            }
+                            case UP: {
+                                if (grid[row - 1][col] == 0) {
+                                    isValid = true;
+                                }
+                                break;
+                            }
+                            case DOWN: {
+                                if (grid[row + 1][col] == 0) {
+                                    isValid = true;
+                                }
+                                break;
+                            }
+                            default:
+                                isValid = false;
+                        }
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
+
+
+
+
+
+
+
+
 }
