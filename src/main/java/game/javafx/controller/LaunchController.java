cad19f45e3ee8c28fda2890398ee8dfaab258c3c
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-09 15:56:29 +0200

    Implementing start and exit buttons

--- src/main/java/game/javafx/controller/LaunchController.java
+++ src/main/java/game/javafx/controller/LaunchController.java
@@ -1,8 +1,17 @@
 package game.javafx.controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LaunchController {
 
@@ -16,6 +25,24 @@ public class LaunchController {
     private Label errorLabel;
 
 
+    public void startGame(ActionEvent actionEvent) throws IOException {
+
+        if (player1name.getText().isEmpty() || player2name.getText().isEmpty()) {
+            errorLabel.setText("Enter your name!");
+        } else {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
+            Parent root = loader.load();
+            GameController gameController = loader.<GameController>getController();
+            gameController.setName(player1name.getText(), player2name.getText());
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+    }
+
+    public void exitGame(ActionEvent actionEvent) {
+        Platform.exit();
+    }
 
 
 
