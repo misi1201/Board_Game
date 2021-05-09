d17ae4031efca9aacfb1243e7075897aa18a3b86
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-10 01:21:32 +0200

    Testing starting positions

--- src/test/java/game/GameModelTest.java
+++ src/test/java/game/GameModelTest.java
@@ -4,7 +4,9 @@ import game.model.GameModel;
 import game.model.Player;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 public class GameModelTest {
@@ -18,6 +20,25 @@ public class GameModelTest {
         gameModel = new GameModel();
         player1 = new Player("p1",1,0);
         player2 = new Player("p2",2,0);
+    }
+
+    @Test
+    void testIsValidMoveAtStartingPosition(){
+
+        assertTrue(gameModel.isValidMove(player1, 0, 0, 3));
+        assertFalse(gameModel.isValidMove(player1, 0, 0, 2));
+        assertFalse(gameModel.isValidMove(player1, 0, 0, 1));
+        assertFalse(gameModel.isValidMove(player1, 0, 0, 0));
+        assertFalse(gameModel.isValidMove(player1, 0, 0, 5));
+        assertFalse(gameModel.isValidMove(player2, 0, 0, 3));
+
+
+
+
+
+
+
+
     }
 
 
