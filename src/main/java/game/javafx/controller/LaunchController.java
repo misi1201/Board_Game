f28129bebf76cae05c7737a703852d5be8247761
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-09 15:41:58 +0200

    Creating LaunchController class

---
+++ src/main/java/game/javafx/controller/LaunchController.java
@@ -0,0 +1,22 @@
+package game.javafx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LaunchController {
+
+    @FXML
+    private TextField player1name;
+
+    @FXML
+    private TextField player2name;
+
+    @FXML
+    private Label errorLabel;
+
+
+
+
+
+}
