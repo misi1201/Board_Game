2812735cafc168a69fe11eef95871a68d8ab483e
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-09 20:59:49 +0200

    Creating two test objects

--- src/test/java/game/GameModelTest.java
+++ src/test/java/game/GameModelTest.java
@@ -2,14 +2,23 @@ package game;
 
 import game.model.GameModel;
 import game.model.Player;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 
+
+@Slf4j
 public class GameModelTest {
 
     GameModel gameModel;
     Player player1;
     Player player2;
 
-
+    @BeforeEach
+    void setUp(){
+        gameModel = new GameModel();
+        player1 = new Player("p1",1,0);
+        player2 = new Player("p2",2,0);
+    }
 
 
 
