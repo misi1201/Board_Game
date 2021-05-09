90355e807e9c6da913fcccc60472905fc648e6f6
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-09 20:27:48 +0200

    Adding basic Test class

---
+++ src/test/java/game/GameModelTest.java
@@ -0,0 +1,16 @@
+package game;
+
+import game.model.GameModel;
+import game.model.Player;
+
+public class GameModelTest {
+
+    GameModel gameModel;
+    Player player1;
+    Player player2;
+
+
+
+
+
+}
