ae0f108ba78b9c8cf867bd3911b184e43ec51e84
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-11 12:22:37 +0200

    Added javadoc to Main.java

--- src/main/java/game/javafx/main/Main.java
+++ src/main/java/game/javafx/main/Main.java
@@ -1,10 +1,18 @@
 package game.javafx.main;
 
-import game.javafx.main.App;
 import javafx.application.Application;
 
+
+/**
+ * The main class that launches the GUI of the application.
+ */
 public class Main {
 
+    /**
+     * The main method is the first one when the application is started.
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
         Application.launch(App.class, args);
     }
