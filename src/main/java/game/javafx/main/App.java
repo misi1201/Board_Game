3d959e808387c54ee603d65c8230aa4f79322f15
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-11 12:28:26 +0200

    Added javadoc to App.java

--- src/main/java/game/javafx/main/App.java
+++ src/main/java/game/javafx/main/App.java
@@ -6,8 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The class that controls the GUI of the application.
+ */
 public class App extends Application {
 
+    /**
+     * The method that starts the first scene of the application.
+     *
+     * @param primaryStage Stage Object
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(App.class.getResource("/fxml/Launch.fxml"));
