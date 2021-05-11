4ec5690cecedd620fb55cf29c402c3cc30765b43
misi1201 <szilagyimihaly2000@gmail.com>
2021-05-11 22:28:45 +0200

    Database support added to the project

---
+++ src/main/java/game/result/GameResultDao.java
@@ -0,0 +1,29 @@
+package game.result;
+
+import com.google.inject.persist.Transactional;
+import util.jpa.GenericJpaDao;
+
+import java.util.List;
+
+/**
+ * DAO class for the {@link GameResult} entity.
+ */
+public class GameResultDao extends GenericJpaDao<GameResult> {
+
+    public GameResultDao() {
+        super(GameResult.class);
+    }
+
+    /**
+     * Returns the list of all results with respect to the steps and time
+     * spent for winning the game.
+     *
+     * @return the list of all results with respect to the steps and time
+     * spent for winning the game.
+     */
+    @Transactional
+    public List<GameResult> findAll() {
+        return entityManager.createQuery("SELECT r FROM GameResult r ORDER BY r.steps ASC, r.duration ASC", GameResult.class).getResultList();
+    }
+
+}
